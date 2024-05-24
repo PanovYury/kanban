05e5b6ee630cff1158624bfe1a87ed0b053d28c5
= <=>
2024-05-25 00:42:18 +0500

    [celery] refactor task signature

--- tasks/service.py
+++ tasks/service.py
@@ -1,20 +1,14 @@
+import httpx
 import logging
 
 from celery import shared_task
-import httpx
-
-from tasks.models import Task
 
 LOGGER = logging.getLogger(__name__)
 
 @shared_task
-def send_notification(task_id: int):
+def send_notification(task: dict):
     """Send notification about update tasks status to user"""
-    task = Task.objects.get(pk=task_id)
     httpx.post('http://centrifugo/api/publish', json={
-        'channel': f'tasks-{task.board_id}',
-        'data': {
-            'task': task.title,
-            'status': task.status
-        }
+        'channel': f'tasks-{task['board']}',
+        'data': task
     })
